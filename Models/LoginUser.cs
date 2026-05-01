01c94b4f926671fdf1c92cd16e4efcc0440044ba
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-05-01 09:07:49 +0200

    add remember me to login

--- Models/LoginUser.cs
+++ Models/LoginUser.cs
@@ -6,4 +6,6 @@ public class LoginUser
 {
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+
+    public bool RememberMe { get; set; }
 }
