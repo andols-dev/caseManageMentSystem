a9c4cc21c6014e2eada8566ab25f98c21ea3a2b1
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-05-30 18:17:31 +0200

    create error handling

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace caseManageMentSystem.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/HandleErrorCode/{statusCode}")]
+        public IActionResult HandleErrorCode(int statusCode)
+        {
+            Response.StatusCode = statusCode;
+
+            if (statusCode == 404)
+            {
+                return View("NotFound");
+            }
+
+            return View("Error", statusCode);
+        }
+
+        public IActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound");
+        }
+    }
+
+
+}
