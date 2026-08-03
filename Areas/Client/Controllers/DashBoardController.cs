f86b208d995c23b572e64a57d1fb1c36e56ae6dc
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-08-03 14:00:54 +0200

    add DashBoardController for admin, casemanager and client areas

---
+++ Areas/Client/Controllers/DashBoardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace caseManageMentSystem.Areas.Client.Controllers
+{
+    [Area("Client")]
+    [Authorize(Roles ="client")]
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
