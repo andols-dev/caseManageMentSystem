5722019c6d0f0c2e994d81554fdafd5d03901731
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-08-03 10:44:00 +0200

    add table: case

---
+++ Models/Case.cs
@@ -0,0 +1,19 @@
+﻿namespace caseManageMentSystem.Models
+{
+    public class Case
+    {
+        public int Id { get; set; }
+
+        public string ClientId { get; set; } = string.Empty;
+        public ApplicationUser Client { get; set; } = null!;
+
+        public string CaseManagerId { get; set; } = string.Empty;
+        public ApplicationUser CaseManager { get; set; } = null!;
+
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
